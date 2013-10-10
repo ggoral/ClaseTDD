ed1eb307c9a0c803e798fbb537d3844769f4a553
Patricio Mac Adden <patriciomacadden@gmail.com>
2013-10-10 18:40:56 -0300

    refactoring!

--- ocr_reader_test.rb
+++ ocr_reader_test.rb
@@ -1,45 +1,42 @@
-#!/usr/bin/env ruby
-# coding: utf-8
-
 require 'minitest/autorun'
+
 require_relative 'ocr_reader'
+
 class OCRReaderTest < Minitest::Test
-  
-  def test_should_initialize_correctly
-    ocr = OCRReader.new 'useCase1'
+  def test_initialize
+    ocr = OCRReader.new 'fixtures/valid'
     assert_instance_of OCRReader, ocr
     assert_instance_of File, ocr.file
   end
 
-  def test_should_not_initialize_correctly
+  def test_initialize_with_invalid_file
     assert_raises Errno::ENOENT do 
       OCRReader.new 'this_is_not_a_file'
     end
   end
-  
-  def test_should_be_valid
-    ocr = OCRReader.new 'useCase1'
+
+  def test_valid_syntax
+    ocr = OCRReader.new 'fixtures/valid'
     assert ocr.valid?
   end
 
-  def test_not_valid
-    ocr = OCRReader.new 'useCase2'
+  def test_invalid_with_less_lines
+    ocr = OCRReader.new 'fixtures/less_lines'
     refute ocr.valid?
   end
 
-  def test_should_validate_line_size
-    ocr = OCRReader.new 'useCase3'
+  def test_invalid_with_less_characters
+    ocr = OCRReader.new 'fixtures/less_characters'
     refute ocr.valid? 
   end	
 
-  def test_should_have_valid_character
-    ocr = OCRReader.new 'useCase4'
+  def test_invalid_with_invalid_chars
+    ocr = OCRReader.new 'fixtures/invalid_chars'
     refute ocr.valid? 
   end
 
-  def test_should_valid_four_line_empty
-    ocr = OCRReader.new 'useCase5'
+  def test_invalid_with_non_empty_last_line
+    ocr = OCRReader.new 'fixtures/non_empty_last_line'
     refute ocr.valid? 
   end
-  
 end 
